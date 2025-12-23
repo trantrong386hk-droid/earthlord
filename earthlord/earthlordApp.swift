3f8f6aaf917313f82e6918ca384977a27be2f5c9
lili <435465210@qq.com>
2025-12-23 21:21:46 +0800

    Initial Commit

---
+++ earthlord/earthlordApp.swift
@@ -0,0 +1,17 @@
+//
+//  earthlordApp.swift
+//  earthlord
+//
+//  Created by lili on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct earthlordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
